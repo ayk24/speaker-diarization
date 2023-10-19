9e45570f59078a137dd93527651b86369c148482
ayk24 <aykueym@gmail.com>
2023-10-19 17:53:10 +0000

    fix: main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import logging
 import os
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import polars as pl
+import pandas as pd
 from dotenv import load_dotenv
+from logzero import logger
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
 
@@ -12,54 +11,58 @@ load_dotenv()
 ROOT_DIR = Path(__file__).parent
 ENV_PATH = Path(ROOT_DIR, ".env")
 AUDIO_PATH = Path(ROOT_DIR, "data/audio.wav")
-OUTPUT_IMAGE_PATH = Path(ROOT_DIR, "outputs/speaker_diarization_result.png")
+OUTPUT_DIARIZATION_PATH = Path(ROOT_DIR, "data/diarization.txt")
 HF_TOKEN = os.environ.get("HF_TOKEN")
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+NUM_SPEAKERS = 9
 
 
 class SpeakerDiarization:
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize SpeakerDiarization class."""
-        self.pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=HF_TOKEN)
+        self.pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
 
-    def run_diarization(self):
+    def run_diarization(self) -> None:
         """Run speaker diarization."""
-        diarization = self.pipeline({"audio": AUDIO_PATH}, num_speakers=9)
-        df = pl.DataFrame(diarization.for_json()["content"])
+        diarization = self.pipeline(AUDIO_PATH, num_speakers=NUM_SPEAKERS)
+        logger.info(f"done: run speaker diarization")
+
+        data = []
+        for turn, _, speaker in diarization.itertracks(yield_label=True):
+            data.append([{"start": turn.start, "end": turn.end}, speaker])
+            logger.info(f"start={turn.start:.1f}s end={turn.end:.1f}s {speaker}")
+
+        df = pd.DataFrame(data, columns=["segment", "label"])
+        self.output_audio_groups(df)
+
+    def output_audio_groups(self, df: pd.DataFrame) -> None:
         audio_segment = AudioSegment.from_file(AUDIO_PATH, format="wav")
 
         for label in sorted(df["label"].unique()):
-            _df = df.filter(pl.col("label") == label)
-            audio_segment_each_speaker = AudioSegment.silent()
+            filtered_df = df[df["label"] == label]
+            audio_segment_each_speaker = AudioSegment.silent(duration=0)
 
-            for _, segment in _df["segment"]:
+            previous_end = 0
+            for _, segment in enumerate(filtered_df["segment"]):
                 start = segment["start"] * 1000
                 end = segment["end"] * 1000
+                duration = start - previous_end
+                audio_segment_each_speaker += AudioSegment.silent(duration=duration)
                 audio_segment_each_speaker += audio_segment[start:end]
+                previous_end = end
+                logger.info(f"dur={duration:.1f}s start={start:.1f}s end={end:.1f}s")
 
-            logger.info(f"{label=}, duration={audio_segment_each_speaker.duration_seconds}")
+            audio_segment_each_speaker += audio_segment[previous_end:]
+
+            logger.info(f"{label}, {audio_segment_each_speaker.duration_seconds}s")
             audio_segment_each_speaker.export(f"outputs/{label}.wav", format="wav")
 
-    def save_diarization_image(self):
-        diarization = self.pipeline({"audio": AUDIO_PATH}, num_speakers=9)
-        plt.figure(figsize=(10, 3))
 
-        for segment, label in diarization.itertracks(yield_label=True):
-            speaker_id = label.split(" ")[0]
-            color = int(speaker_id) % 10
-            plt.barh(0, segment.duration, left=segment.start, color=plt.cm.tab10(color), label=speaker_id)
-
-        plt.yticks([])
-        plt.savefig(OUTPUT_IMAGE_PATH, bbox_inches="tight", pad_inches=0.1)
-        plt.show()
-
-
-def main():
+def main() -> None:
     speaker_diarization = SpeakerDiarization()
-    speaker_diarization.save_diarization_image()
+    logger.info("done: initialize SpeakerDiarization class")
+
     speaker_diarization.run_diarization()
+    logger.info("done: run speaker diarization")
 
 
 if __name__ == "__main__":
